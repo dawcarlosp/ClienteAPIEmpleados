e9d02a1715886799f4725c16299008447bcd1341
Carlos P <dawcarlospereira@gmail.com>
2025-02-12 18:12:46 +0100

    Eliminar no funcionaba correctamente porque estaba comiendo el td que precedia al button

--- js/app.js
+++ js/app.js
@@ -1,36 +1,37 @@
 async function obtenerProyectos() {
   try {
+    document.getElementById("TableBody").innerHTML = "";
     const response = await fetch("http://localhost:8080/proyectos");
     if (!response.ok) {
       throw new Error("Error al obtener los proyectos");
     }
     const proyectos = await response.json();
     console.log(proyectos);
-    document.getElementById("TableBody").innerHTML = "";
     proyectos.forEach(proyecto => {
       const fila = document.createElement("tr");
       fila.setAttribute("data-id",proyecto.id);
-      fila.innerHTML = `<td>${proyecto.nombre}</td><td>${proyecto.descripcion}</td><td>${proyecto.presupuesto}</td><td>${proyecto.fechaInicio}</td><button class="btn btn-danger btn-sm" onclick="borrarProyecto(${proyecto.id}, this)">Borrrar</button></td>`;
+      fila.innerHTML = `<td>${proyecto.nombre}</td><td>${proyecto.descripcion}</td><td>${proyecto.presupuesto}</td><td>${proyecto.fechaInicio}</td><td><button class="btn btn-danger btn-sm" onclick="borrarProyecto(${proyecto.id},this)">Borrrar</button></td>`;
       document.getElementById("TableBody").appendChild(fila);
     })
   } catch (error) {
     console.error("Error:", error);
   }
 }
+obtenerProyectos();
 async function borrarProyecto(id, elemento){
     try{
         const response = await fetch(`http://localhost:8080/proyectos/${id}`, {method: 'DELETE'});
         //obtenerProyectos();
+        elemento.parentNode.parentNode.remove();
         if(!response.ok){
             throw new Error(`Error al eliminar el proyecto ${id}`);
         }
-        elemento.parentNode.parentNode.remove();
     }catch(error){
-
+        console.error("Error:", error);
     }
 }
 
-obtenerProyectos();
+
 /*
 fetch("http://localhost:8080/proyectos")
   .then((response) => response.json())
